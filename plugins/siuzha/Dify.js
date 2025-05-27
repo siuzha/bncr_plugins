30a2d7e931b6d2b1d8c8cdf993f8323ffa8495d3
siuzha <117625679+siuzha@users.noreply.github.com>
2025-05-27 09:18:06 +0800

    Update Dify.js

--- plugins/siuzha/Dify.js
+++ plugins/siuzha/Dify.js
@@ -3,7 +3,7 @@
  * @name Dify
  * @team siuzha
  * @version 1.0.1
- * @description Dify聊天插件，触发方法：ai/AI [内容] 或 艾特机器人 [内容]。"清空对话"或"删除对话"清空对话历史
+ * @description Dify聊天插件，触发方法：ai/AI [内容] 或 艾特机器人 [内容]。"清空对话"或"删除对话"清空对话历史。
  * @rule ^(ai\s|AI\s|@\S+\s)
  * @admin false
  * @public true
